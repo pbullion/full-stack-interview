1db25ad279ae14c86edb59c6cf9947e078b0c9af
Patrick Bullion <pbullion@gmail.com>
2020-12-31 10:44:00 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ WORKDIR /app/lbm_mrt
 
 RUN apt-get update && apt-get install -y make
 
-#RUN apt-get update && apt-get install -y emacs24
-
 
--- aws/batch/index.js
+++ aws/batch/index.js
@@ -7,9 +7,5 @@ const myBucket = process.env.MY_BUCKET;
 const myKey = process.env.MY_KEY;
 const jobId = `${process.env.AWS_BATCH_JOB_ID}/`;
 const params = { Bucket: myBucket, Key: jobId };
-console.log('myBucket:', myBucket);
-console.log('jobId:', jobId);
-console.log('myKey:', myKey);
-console.log('params:', params);
 
 s3.putObject(params).promise();
